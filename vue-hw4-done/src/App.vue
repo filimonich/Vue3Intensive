708ef2b751c7e6aacdfaac265f904578e4416eeb
Sergey Filimonov <srfilimonoff@gmail.com>
2023-05-21 20:55:10 +0300

    add lesson 4

---
+++ vue-hw4-done/src/App.vue
@@ -0,0 +1,99 @@
+<template>
+	<div class="container mt-2">
+		<form @submit.prevent="sendForm" v-if="!formDone">
+			<app-progress :current="fieldDone" :max="info.length" />
+			<div>
+				<app-field 
+					v-for="field,i in info"
+					:key="i"
+					:label="field.label"
+					:value="field.value"
+					:valid="field.valid"
+					@updated="onUpdate(i, $event)"
+				></app-field>
+			</div>
+			<button class="btn btn-primary" :disabled="!formReady">
+				Send Data
+			</button>
+		</form>
+		<div v-else>
+			<table class="table table-bordered">
+				<tbody>
+					<tr v-for="field,i in info" :key="i">
+						<td>{{ field.label }}</td>
+						<td>{{ field.value }}</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</template>
+
+<script>
+import AppField from './components/Field'
+import AppProgress from './components/Progress'
+
+export default {
+	components: { AppField, AppProgress },
+	data: () => ({
+		info: [
+			{
+				label: 'Name',
+				value: '',
+				pattern: /^[a-zA-Z ]{2,30}$/
+			},
+			{
+				label: 'Phone',
+				value: '',
+				pattern: /^[0-9]{7,14}$/
+			},
+			{
+				label: 'Email',
+				value: '',
+				pattern: /.+/
+			},
+			{
+				label: 'Some Field 1',
+				value: '',
+				pattern: /.+/
+			},
+			{
+				label: 'Some Field 2',
+				value: '',
+				pattern: /.+/
+			}
+		],
+		formDone: false
+	}),
+	computed: {
+		fieldDone(){
+			// return this.info.reduce((total, field) => total + (field.valid ? 1 : 0), 0)
+			return this.info.filter(field => field.valid).length;
+		},
+		formReady(){
+			return this.fieldDone >= this.info.length;
+		},
+		progressStyles(){
+			let rel = this.fieldDone / this.info.length * 100;
+			return { width: rel + '%' };
+		}
+	},
+	methods: {
+		onUpdate(i, val){
+			let field = this.info[i];
+			field.value = val.trim();
+			field.valid = field.pattern.test(field.value);
+		},
+		sendForm(){
+			if(this.formReady){
+				this.formDone = true;
+			}
+		}
+	},
+	created(){
+		this.info.forEach(field => {
+			field.valid = false;
+		})
+	}
+}
+</script>
---
+++ vue-hw4-done/src/components/Field.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="form-group">
+		<label>
+			{{ label }}
+			<fa-icon v-if="activated"
+				:class="valid ? 'text-success' : 'text-danger'"
+				:icon="valid ? 'check-circle' : 'exclamation-circle'"
+			/>
+		</label>
+		<input type="text" class="form-control" :value="value" @input="onInput">
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		label: { type: String, required: true },
+		value: { type: String, required: true },
+		valid: { type: Boolean, required: true }
+	},
+	data(){
+		return {
+			activated: this.value != ''
+		}
+	},
+	methods: {
+		onInput(e){
+			this.activated = true;
+			this.$emit('updated', e.target.value)
+		}
+	}
+}
+</script>
