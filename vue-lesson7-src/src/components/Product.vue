e8abded65461414a8b92a1dddd48d655bb0f1543
Sergey Filimonov <srfilimonoff@gmail.com>
2023-05-23 20:03:28 +0300

    add lesson 7

---
+++ vue-lesson7-src/src/components/Product.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Title</h1>
+    Back to products
+    <hr />
+    <div class="alert alert-success">
+      price
+      {{ $route.params.id }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
